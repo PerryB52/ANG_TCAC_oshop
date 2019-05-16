45e2ad6e2eb6272d6c95ea089a82ef6711be4323
Alexandru Pereteanu <alexandru.pereteanu@infoworld.ro>
2019-05-16 18:12:02 +0300

    finalized google login, logut, current user - section 20 v289

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '../../../node_modules/angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
-  constructor() { }
+  user: firebase.User;
 
-  ngOnInit() {
+  constructor(private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user => this.user = user);
   }
 
+  logout(){
+    this.afAuth.auth.signOut();
+  }
 }
