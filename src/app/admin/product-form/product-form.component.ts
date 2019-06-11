bbaf68d439cd9602d3fca87ec4ba42e0eef92c50
Alexandru Pereteanu <alexandrup@softinfo.ro>
2019-06-11 23:16:58 +0300

    s21 v308 updating a product

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -13,6 +13,7 @@ export class ProductFormComponent implements OnInit {
 
   categories$;
   product = {};   //using this to avoid errors when loading the empty form
+  id;
 
   constructor(
     private router: Router,
@@ -22,13 +23,14 @@ export class ProductFormComponent implements OnInit {
 
     this.categories$ = categoryService.getCategories();
     
-    let id = this.route.snapshot.paramMap.get('id');
-    if(id) this.productService.get(id).take(1).subscribe(p => this.product = p)
+    this.id = this.route.snapshot.paramMap.get('id');
+    if(this.id) this.productService.get(this.id).take(1).subscribe(p => this.product = p)
     
   }
 
   save(product){
-    this.productService.create(product);
+    if(this.id) this.productService.update(this.id, product);
+    else this.productService.create(product);
     this.router.navigate(['/admin/products'])
   }
 
