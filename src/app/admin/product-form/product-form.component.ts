249a299dea4c3a7c67399f1c7b96677bcb5f08bf
Alexandru Pereteanu <alexandrup@softinfo.ro>
2019-07-29 22:39:55 +0300

    s22 v322 dealing with multiple async opps

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -18,14 +18,14 @@ export class ProductFormComponent implements OnInit {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private categoryService: CategoryService, 
+    private categoryService: CategoryService,
     private productService: ProductService) {
 
-    this.categories$ = categoryService.getCategories();
-    
+    this.categories$ = categoryService.getAll();
+
     this.id = this.route.snapshot.paramMap.get('id');
     if(this.id) this.productService.get(this.id).take(1).subscribe(p => this.product = p)
-    
+
   }
 
   save(product){
@@ -36,7 +36,7 @@ export class ProductFormComponent implements OnInit {
 
   delete(){
     if(!confirm('Are you sure you want to delete this product?')) return;
-    
+
     this.productService.delete(this.id);
     this.router.navigate(['/admin/products'])
   }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
+import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
+import { switchMap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  categories$;
+  category: string;
+
+  constructor(route: ActivatedRoute,
+    productService: ProductService,
+    categoryService: CategoryService) {
+
+    productService
+      .getAll().pipe(
+        switchMap(products => {
+          this.products = products;
+          return route.queryParamMap;
+        }))
+      .subscribe(params => {
+        this.category = params.get('category');
+
+        this.filteredProducts = (this.category) ?
+          this.products.filter(p => p.category === this.category) :
+          this.products;
+      });
+
+    this.categories$ = categoryService.getAll();
+
+  }
+}
+
+/*
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-products',
@@ -8,9 +54,32 @@ import { ProductService } from '../product.service';
 })
 export class ProductsComponent {
 
-  products$;
+  products: Product[] = []; //good practice always initialize arrays.
+  filteredProducts: Product[];
+  categories$;
+  category: string;
 
-  constructor(productService: ProductService) {
-    this.products$ = productService.getAll();
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService,
+    categoryService: CategoryService) {
+
+    productService
+      .getAll()
+      .switchMap(products => {
+        this.products = products;
+
+        return route.queryParamMap;
+      })
+
+      .subscribe(params => {
+        this.category = params.get('category');
+        this.filteredProducts = (this.category) ?
+          this.products.filter(p => p.category === this.category) :
+          this.products;
+      });
+
+    this.categories$ = categoryService.getAll();
    }
 }
+*/
