708264b75fa7be22487afa9d879dbf3b427e6419
Alexandru Pereteanu <alexandrup@softinfo.ro>
2019-08-26 23:03:52 +0300

    s23 v338 finalized building a shopping cart page

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -3,21 +3,30 @@ import * as firebase from 'firebase';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../auth.service';
 import { AppUser } from '../models/app-user';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { ShoppingCart } from '../models/shopping-cart';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent {
+export class BsNavbarComponent implements OnInit {
 
   appUser: AppUser;
+  cart$: Observable<ShoppingCart>;
 
-  constructor(private auth: AuthService) {
-    auth.appUser$.subscribe(appUser => this.appUser = appUser)
+  constructor(private auth: AuthService, private shoppingCartService: ShoppingCartService) {
+
+  }
+
+  async ngOnInit() {
+    this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
+    this.cart$ = await this.shoppingCartService.getCart();
   }
 
   logout(){
     this.auth.logout();
   }
+
 }
--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 import { Product } from './models/product';
 import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/map';
+import { ShoppingCart } from './models/shopping-cart';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,9 +19,10 @@ export class ShoppingCartService {
     });
   }
 
-  async getCart() {
+  async getCart(): Promise<Observable<ShoppingCart>> {
     let cartId = await this.getOrCreateCartId();
     return this.db.object('/shopping-carts/' + cartId)
+          .map(x => new ShoppingCart(x.items));
   }
 
   private getItem(cartId: string, productId: string) {
@@ -54,181 +58,3 @@ export class ShoppingCartService {
     });
   }
 }
-
-
-
-
-/*
-/*
- /*
-  ///check
-  // https://www.udemy.com/the-complete-angular-master-class/learn/lecture/7859962#questions/7773570
-  private getItem(cartId, productId) {
-    return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
-  }
-
-  async addToCart(product: Product) {
-    let cartId = await this.getOrCreateCartId();
-    let item$ = this.getItem(cartId, product.$key);
-
-    item$.snapshotChanges().pipe(take(1)).subscribe((item: any) => {
-
-      console.log(item.payload.data());
-
-
-      if (!item.payload.exists) {
-        item$.set({
-          product: product,
-          quantity: 1
-        });
-      } else {
-        item$.update({
-          quantity: item.payload.data().quantity + 1
-        });
-      }
-    });
-
-  }
-*/
-
-/*
-async addToCart(product) {
-  let cartId = await this.getOrCreateCartId();
-  let item$ = this.db.object('/shopping-carts/' + cartId + '/items/' + product.key);
-  item$.snapshotChanges().pipe(take(1)).subscribe((item: any) => {
-
-    if (item.payload.val()) {
-      item$.update({
-        quantity: item.payload.val().quantity + 1
-      })
-    }
-
-    else {
-      item$.update({
-        product: product.payload.val(),
-        quantity: 1
-      })
-    }
-  })
-}
-*/
-//https://stackoverflow.com/questions/53247964/property-pipe-does-not-exist-on-type-angularfireobject
-
-
-/*
-async addToCart(product) {
-  let cartId = await this.getOrCreateCartId();
-  let item$ = this.db.object('/shopping-carts/' + cartId + '/items/' + product.key);
-  item$.snapshotChanges().pipe(take(1)).subscribe((item: any) => {
-
-    if (item.payload.val()) {
-      item$.update({
-        quantity: item.payload.val().quantity + 1
-      })
-    }
-
-    else {
-      item$.update({
-        product: product.payload.val(),
-        quantity: 1
-      })
-    }
-  })
-}
-
-
-/*
-async addToCart(product: Product) {
-  let cartId = await this.getOrCreateCartId();
-  let itemRef = this.db.object('/shopping-carts/' + cartId + '/items/' + product.$key);
-  let item$ = itemRef.snapshotChanges();
-
-  item$.pipe(take(1)).subscribe(item => {
-    if (item.payload.exists()) itemRef.update({ quantity: item.payload.val()['quantity'] + 1 });
-    else itemRef.set({ product: product, quantity: 1 });
-  })
-}
-*/
-
-
-
-/*
-export class ShoppingCartService {
-
-  constructor(private db: AngularFireDatabase) { }
-
-  private create() {
-    return this.db.list('/shopping-carts').push({
-      dateCreate: new Date().getTime()
-    });
-  }
-
-  private getCart(cartId: string) {
-    return this.db.object('/shopping-carts/' + cartId)
-  }
-
-  //async anotation - decorating with async means method will return a promise
-  private async getOrCreateCartId() {
-
-    let cartId = localStorage.getItem('cartId');
-    if (cartId) return cartId;
-
-    let result = await this.create(); //calling asnyc metod like a synchronous method using "await" operator
-    localStorage.setItem('cartId', result.key);
-    return result.key;
-
-  }
-
-  async addToCart(product: Product) {
-    let cartId = await this.getOrCreateCartId();
-    let item$ = this.db.object('/shopping-carts/' + cartId + '/items/' + product.$key);
-
-    item$.take(1).subscribe(item => {
-      if (item.$exists()) item$.update({ quantity: item.quantity + 1 });
-      else item$.set({ product: product, qunatity: 1 });
-    })
-
-
-  }
-
-  async addToCart2(product: Product) {
-    let cartId = await this.getOrCreateCartId();
-    let item$ = this.db.object('/shopping-carts/' + cartId + '/items/' + product.$key);
-
-    item$.take(1).subscribe(item => {
-      if (item.payload.val()) {
-        item$.update({
-          quantity: item.payload.val().quantity + 1
-        })
-      }
-
-      else {
-        item$.update({
-          product: product.payload.val(),
-          quantity: 1
-        })
-      }
-    })
-
-  }
-
-  private async updateItem(product: Product, change: number) {
-    let cartId = await this.getOrCreateCartId();
-    let item$ = this.db.object('/shopping-carts/' + cartId + '/items/' + product.$key);
-    item$.take(1).subscribe(item => {
-      let quantity = (item.['quantity'] || 0) + change;
-      if (quantity === 0) item$.remove();
-      else item$.update({
-        title: product.title,
-        imageUrl: product.imageUrl,
-        price: product.price,
-        quantity: quantity
-      });
-    });
-  }
-
-
-
-}
-
-*/
