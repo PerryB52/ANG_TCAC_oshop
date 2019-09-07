574d92e65f44fbd09f6fb65ad96d15f5bbb85dd2
Alexandru Pereteanu <alexandrup@softinfo.ro>
2019-09-07 19:54:23 +0300

    s23 v340 Displaying the total price

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -2,11 +2,15 @@ import { ShoppingCartItem } from "./shopping-cart-item";
 
 export class ShoppingCart {
 
-    constructor(public items: ShoppingCartItem[]) { }
+    items: ShoppingCartItem[] = [];
 
-    get productIds() {
-        return Object.keys(this.items);
-    }
+    constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
+        for (let productId in itemsMap) {
+            let item = itemsMap[productId]
+            this.items.push(new ShoppingCartItem(item.product, item.quantity))
+        }
+
+     }
 
     get totalItemsCount() {
         let count = 0;
@@ -14,6 +18,14 @@ export class ShoppingCart {
             count += this.items[productId].quantity
 
         return count;
+    }
+
+    get totalPrice() {
+        let sum = 0;
+        for (let productId in this.items)
+            sum += this.items[productId].totalPrice
+
+        return sum;
     }
 
 }
