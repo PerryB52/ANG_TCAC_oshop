88fbcf898d817864cf512f4e7131722c0fefef1d
Alexandru Pereteanu <alexandrup@softinfo.ro>
2019-09-15 14:10:54 +0300

    update angular fire database

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -6,14 +6,24 @@ export class ShoppingCart {
     items: ShoppingCartItem[] = [];
 
     constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
+        //in order to make sure the itemsMap field is initialized properly
+        this.itemsMap = itemsMap || {};
+
         for (let productId in itemsMap) {
             let item = itemsMap[productId]
-            this.items.push(new ShoppingCartItem(item.product, item.quantity))
+            this.items.push(new ShoppingCartItem({
+                // title: item.title,
+                // imageUrl: item.imageUrl,
+                // price: item.price,
+                ...item, //this line is equivalent to the above 3 lines, typescript object spread operator.
+                $key: productId
+            }));
         }
 
      }
 
     getQuantity(product: Product) {
+        console.log("product: ", product)
         let item = this.itemsMap[product.$key]
         return item ? item.quantity : 0;
     }
